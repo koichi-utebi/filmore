d052498fbb9b69260b9b5ae166406d1768a38a0c
“koichi-utebi <k.utebi.1225@gmail.com>
2022-06-05 06:38:32 +0000

    Add gem

--- Gemfile
+++ Gemfile
@@ -54,3 +54,13 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'devise'
+
+gem 'kaminari','~> 1.2.1'
+
+gem 'enum_help'
+
+gem 'themoviedb-api', '1.3'
+
+gem 'dotenv-rails'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "ddc2eefc7e62449ebb65d91ac5cd6863.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
