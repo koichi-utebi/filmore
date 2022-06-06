0fe85b96338da9a07a462eef4abf86687e4e7902
“koichi-utebi <k.utebi.1225@gmail.com>
2022-06-06 06:06:37 +0000

    Add homes top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,6 +3,7 @@ class Public::HomesController < ApplicationController
   Tmdb::Api.key(ENV['API_KEY'])
   Tmdb::Api.language("ja")
   def top
+    @movies = JSON.parse((Tmdb::Movie.popular).to_json)
   end
 
   def about
--- app/controllers/public/movies_controller.rb
+++ app/controllers/public/movies_controller.rb
@@ -1,5 +1,10 @@
 class Public::MoviesController < ApplicationController
+  require 'themoviedb-api'
+  Tmdb::Api.key(ENV['API_KEY'])
+  Tmdb::Api.language("ja")
   def index
+    @movies = JSON.parse((Tmdb::Movie.popular).to_json)
+    @movie_info = JSON.parse((Tmdb::Discover.movie).to_json)
   end
 
   def show
